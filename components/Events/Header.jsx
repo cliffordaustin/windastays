53c60ef31a46488f24d645d7930b2565c8087f02
cliffordaustin <cliffordaustin670@gmail.com>
2023-01-24 20:57:55 +0000

    first commit

---
+++ components/Events/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Header(props) {
+  return <div className="relative"></div>;
+}
+
+Header.propTypes = {};
+
+export default Header;
---
+++ pages/events/kaleidoscope/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function EventKaleidoscope(props) {
+  return <div></div>;
+}
+
+EventKaleidoscope.propTypes = {};
+
+export default EventKaleidoscope;
